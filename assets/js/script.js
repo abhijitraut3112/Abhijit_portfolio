9e60672eff26cf4fc0ebf88d700a4a3778b3ef61
Abhijit Raut <88086379+abhijitraut3112@users.noreply.github.com>
2023-07-20 21:53:04 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,7 +76,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["Frontend Developer ", "Backend Developer", "", "fullstack java developer "],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
